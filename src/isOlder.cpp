fdcc635d9656e478506944096d2d0efdc3037ed0
aditya-3 <aditya-3@missionrnd.com>
2016-03-02 23:49:36 +0530

    Submit Commit

--- src/isOlder.cpp
+++ src/isOlder.cpp
@@ -13,6 +13,10 @@ ERROR CASES: Return -1 if any DOB is invalid.
 NOTES: 		Don't use any built-in C functions for comparisions. You are free to write any helper functions.
 */
 #include<stdlib.h>
+
+
+//converting date1 to a number
+
 void convert(char *dob,int **a,int k)
 {
 	int i = 0,j=0,sum=0,t;
@@ -33,9 +37,74 @@ void convert(char *dob,int **a,int k)
 	}
 	a[k][j] = sum;
 }
+
+
+int check_other_symbols(char *dob)
+{
+	int i = 0;
+	while (dob[i] != '\0')
+	{
+		if (i != 2 && i != 5)
+		{
+
+			if (dob[i] < 48 || dob[i]>57)
+				return -1;
+		}
+		i++;
+	}
+	return 0;
+}
+
+//checking for any leap year
+
+void check_leapYear(int year, int *flag)
+{
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+				*flag = 1;
+		}
+		else
+			*flag = 1;
+
+	}
+}
+
+//checking for valid date or not for date2
+
+int check_date(int month,int day,int flag)
+{
+	if (month > 0 && month < 13)
+	{
+
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		{
+			if (day > 31)
+				return -1;
+		}
+		else if (month == 2)
+		{
+			if ((flag == 0 && day > 28) || (flag == 1 && day > 29))
+				return -1;
+		}
+		else {
+
+			if (day > 30)
+				return -1;
+		}
+	}
+	else
+		return -1;
+	return 0;
+}
+
+//given function
+
 int isOlder(char *dob1, char *dob2)
 {
-	int *a[2], i = 0, j = 0, t, sum = 0, dob1flag = 0, dob2flag = 0;
+	int *a[2], i = 0, j = 0, t, sum = 0, dob1flag = 0, dob2flag = 0,day1,day2,mon1,mon2,year1,year2;
 	for (i = 0; i<2; i++)
 	{
 		a[i] = (int *)malloc(3 * sizeof(int));
@@ -46,147 +115,48 @@ int isOlder(char *dob1, char *dob2)
 
 		return -1;
 	}
-	i = 0;
-	//chekcing for any extra characters except numbers and '-' in date1
-	while (dob1[i] != '\0')
-	{
-		if (i != 2 && i != 5)
-		{
+		
+	//chekcing for any extra characters except numbers and '-' in date
+	t = check_other_symbols(dob1);
+	if (t == -1)
+		return -1;
+	t=check_other_symbols(dob2);
+	if (t == -1)
+		return -1;
+	
+	
+	convert(dob1, a, 0);     //converting date1 to a number
+	day1 = a[0][0]; mon1 = a[0][1]; year1 = a[0][2];
+	
+	 convert(dob2,a,1);       //converting date2 to a number
+	 day2 = a[1][0]; mon2 = a[1][1]; year2 = a[1][2];
 
-			if (dob1[i] < 48 || dob1[i]>57)
-				return -1;
-		}
-		i++;
-	}
-	i = 0;
-	//chekcing for any extra characters except numbers and '-' in date2
-	while (dob2[i] != '\0')
-	{
-		if (i != 2 && i != 5)
-		{
+	
+	 check_leapYear(year1, &dob1flag);      //checking for any leap year for date 1
+	
+	 check_leapYear(year2, &dob2flag);      //checking for any leap year for date 2
+	
 
-			if (dob2[i] < 48 || dob2[i]>57)
-				return -1;
-		}
-		i++;
-	}
-	i = 0;
-	//converting date1 to a number
-	/*while (dob1[i] != '\0')
-	{
-		if (dob1[i] != '-')
-		{
-			t = dob1[i] - 48;
-			sum *= 10;
-			sum = sum + t;
-		}
-		else if (dob1[i] == '-')
-		{
-			a[0][j++] = sum;
-			sum = 0;
-		}
-		i++;
-	}
-	a[0][j] = sum;
-	j = 0;
-	i = 0;
-	t = 0;
-	sum = 0;*/
-	convert(dob1, a, 0);
-	//converting date2 to a number
-	 convert(dob2,a,1);
-	/*while (dob2[i] != '\0')
-	{
-		if (dob2[i] != '-')
-		{
-			t = dob2[i] - 48;
-			sum *= 10;
-			sum = sum + t;
-		}
-		else if (dob2[i] == '-')
-		{
-			a[1][j++] = sum;
-			sum = 0;
-		}
-		i++;
-	}
-	a[1][j] = sum;*/
-
-	//checking for any leap year for date 1
-	if (a[0][2] % 4 == 0)
-	{
-		if (a[0][2] % 100 == 0)
-		{
-			if (a[0][2] % 400 == 0)
-				dob1flag = 1;
-		}
-		else
-			dob1flag = 1;
-
-	}
-	//checking for any leap year for date 2
-	if (a[1][2] % 4 == 0)
-	{
-		if (a[1][2] % 100 == 0)
-		{
-			if (a[1][2] % 400 == 0)
-				dob2flag = 1;
-		}
-		else
-			dob2flag = 1;
-
-	}
-
-	/////checking for valid date or not for date1
-	if (a[0][1] == 1 || a[0][1] == 3 || a[0][1] == 5 || a[0][1] == 7 || a[0][1] == 8 || a[0][1] == 10 || a[0][1] == 12)
-	{
-
-		if (a[0][0] > 31)
-			return -1;
-	}
-	else if (a[0][1] == 2)
-	{
-		if ((dob1flag == 0 && a[0][0] > 28) || (dob1flag == 1 && a[0][0] > 29))
-		{
-
-			return -1;
-		}
-	}
-	else {
-
-		if (a[0][0] > 30)
-			return -1;
-	}
-	///checking for valid date or not for date2
-	if (a[1][1] == 1 || a[1][1] == 3 || a[1][1] == 5 || a[1][1] == 7 || a[1][1] == 8 || a[1][1] == 10 || a[1][1] == 12)
-	{
-
-		if (a[1][0] > 31)
-			return -1;
-	}
-	else if (a[1][1] == 2)
-	{
-
-		if ((dob2flag == 0 && a[1][0] > 28) || (dob2flag == 1 && a[1][0] > 29))
-			return -1;
-
-	}
-	else {
-		if (a[1][0] > 30)
-			return -1;
-	}
-	///checking for same date or not
-	if (a[0][2] == a[1][2] && a[0][1] == a[1][1] && a[0][0] == a[1][0])
+	
+	 t = check_date(mon1,day1,dob1flag);     /////checking for valid date or not for date1
+	 if (t == -1)
+		 return -1;
+	 t = check_date(mon2, day2, dob2flag);    /////checking for valid date or not for date2
+	 if (t == -1)
+		 return -1;
+	 
+	//checking for same date or not
+	if (year1 == year2 && mon1 == mon2 && day1 == day2)
 	{
 
 		return 0;
 	}
 
-	else if (a[0][2] <= a[1][2])
+	else if (year1 <= year2)
 	{
-		if (a[0][1] <= a[1][1])
+		if (mon1 <= mon2)
 		{
-			if (a[0][0] <= a[1][0])
+			if (day1 <= day2)
 			{
 				
 				return 1;
